facf54bc7e3c6cd5a502e11eea477bf56cea02d9
Mansi Yadav <139609682+FreeSpirit11@users.noreply.github.com>
2024-03-23 09:54:57 +0530

    Add files via upload

---
+++ 02. tip-calculator-start/main.py
@@ -0,0 +1,13 @@
+#If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+bill=150.00
+new_amount=150*1.12
+each_person_bill=new_amount/5
+bill_pay=round(each_person_bill,0)
+print(bill_pay)
